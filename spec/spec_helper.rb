48d76dd847e23b033a486a8dc0dd04f2f59b6571
kucaahbe <kucaahbe@ukr.net>
2013-07-16 23:13:14 +0300

    rewritten raise_spec_error helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,18 +25,44 @@ module AssetHelpers
 
 end
 
-module CustomHelpers
-
-  def raise_spec_error msg
-    raise_error RSpec::Expectations::ExpectationNotMetError, msg
+RSpec::Matchers.define :raise_spec_error do |expected_exception_msg|
+  define_method :actual_msg do
+    @actual_msg
   end
 
+  match do |block|
+    begin
+      block.call
+      false
+    rescue RSpec::Expectations::ExpectationNotMetError => rspec_error
+      @actual_msg = rspec_error.message
+
+      case expected_exception_msg
+      when String
+        actual_msg == expected_exception_msg
+      when Regexp
+        actual_msg =~ expected_exception_msg
+      end
+    end
+  end
+
+  failure_message_for_should do |block|
+    <<MSG
+expected RSpec::Expectations::ExpectationNotMetError with message:
+#{expected_exception_msg}
+
+got:
+#{actual_msg}
+
+Diff:
+#{RSpec::Expectations::Differ.new.diff_as_string(actual_msg,expected_exception_msg.to_s)}
+MSG
+  end
 end
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
   config.extend  AssetHelpers
-  config.include CustomHelpers
   config.include RSpec::Html::Matchers
 end
