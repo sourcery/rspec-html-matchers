88b83ca2253bfe6d0209c817d6045211bda6474e
kucaahbe <kucaahbe@ukr.net>
2013-07-16 23:46:26 +0300

    improved raise_spec_error helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,6 +30,10 @@ RSpec::Matchers.define :raise_spec_error do |expected_exception_msg|
     @actual_msg
   end
 
+  define_method :catched_exception do
+    @catched_exception
+  end
+
   match do |block|
     begin
       block.call
@@ -43,11 +47,15 @@ RSpec::Matchers.define :raise_spec_error do |expected_exception_msg|
       when Regexp
         actual_msg =~ expected_exception_msg
       end
+    rescue StandardError => e
+      @catched_exception = e
+      false
     end
   end
 
   failure_message_for_should do |block|
-    <<MSG
+    if actual_msg
+<<MSG
 expected RSpec::Expectations::ExpectationNotMetError with message:
 #{expected_exception_msg}
 
@@ -57,6 +65,11 @@ got:
 Diff:
 #{RSpec::Expectations::Differ.new.diff_as_string(actual_msg,expected_exception_msg.to_s)}
 MSG
+    elsif catched_exception
+      "expected RSpec::Expectations::ExpectationNotMetError, but was #{catched_exception.inspect}"
+    else
+      "expected RSpec::Expectations::ExpectationNotMetError, but was no exception"
+    end
   end
 end
 
